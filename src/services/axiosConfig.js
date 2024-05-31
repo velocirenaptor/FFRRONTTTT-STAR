4c35f88e04a4f71b766d421e6e5e8beb0307f7ee
harlock024 <hadith024@gmail.com>
2024-05-31 00:08:53 -0600

    back connection

---
+++ src/services/axiosConfig.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import { error } from 'jquery';
+
+const axiosConfig = axios.create({
+    baseURL: 'http://localhost:3001/api/',
+    headers: {
+        'Content-type': 'application/json'
+    }
+});
+
+axiosConfig.interceptors.request.use(config =>{
+
+    const token = localStorage.getItem('accessToken');
+    if(token){
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+}
+);
+
+axiosConfig.interceptors.response.use(
+    response => response,
+        error =>{
+            if(error.response && error.response.status === 401){
+                window.location = '/login';
+            }
+            return Promise.reject(error);
+        }
+)
+
+export default axiosConfig;
